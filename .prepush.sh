9749d874f2fe859c554e2c306107b3e3559e6750
MaryAlex <worker96@mail.ru>
2017-11-22 21:40:07 +0300

    precommit -> prepush

--- .prepush.sh
+++ .prepush.sh
@@ -1,7 +1,8 @@
+#!/usr/bin/env bash
 # Disallow pushing into master
 branch=`git symbolic-ref HEAD`
 if [ "$branch" = "refs/heads/master" ]; then
-    echo "Direct commits to the branch master are not allowed"
+    echo "Direct push to the branch master are not allowed"
     exit 1
 fi
 
